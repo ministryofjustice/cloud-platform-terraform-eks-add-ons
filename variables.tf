881f38030889dd673a21c174982138a363b8db4f
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-12-01 17:58:21 +0000

    Update default versions to AWS defaults fetchec from cli

--- variables.tf
+++ variables.tf
@@ -26,18 +26,21 @@ variable "addon_create_coredns" {
 }
 
 variable "addon_vpc_cni_version" {
-  default     = "v1.9.3-eksbuild.1"
+  default     = "v1.7.5-eksbuild.2"
   description = "Version for addon_create_vpc_cni"
+  type = string
 }
 
 variable "addon_kube_proxy_version" {
-  default     = "v1.19.6-eksbuild.1"
+  default     = "v1.19.8-eksbuild.1"
   description = "Version for addon_kube_proxy_version"
+  type = string
 }
 
 variable "addon_coredns_version" {
   default     = "v1.8.0-eksbuild.1"
   description = "Version for addon_coredns_version"
+  type = string
 }
 
 variable "cluster_oidc_issuer_url" {

da2e0af34b344b67fc344841945b7f084f53f677
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2021-12-01 13:04:10 +0000

    Fix conditions for role
    This is identified while running plan

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "irsa_vpc_cni" {
   create_role                   = var.addon_create_vpc_cni ? true : false
   role_name                     = "${var.cluster_name}-vpc-cni"
   provider_url                  = var.cluster_oidc_issuer_url
-  role_policy_arns              = [aws_iam_policy.vpc_cni.arn, "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"]
+  role_policy_arns              = [aws_iam_policy.vpc_cni[0].arn, "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:aws-node"]
 }
 
@@ -17,7 +17,7 @@ resource "aws_iam_policy" "vpc_cni" {
 
   name        = "${var.cluster_name}-vpc-cni"
   description = "EKS cluster addon for VPC CNI ${var.cluster_name}"
-  policy      = data.aws_iam_policy_document.vpc_cni.json
+  policy      = data.aws_iam_policy_document.vpc_cni[0].json
   lifecycle {
     ignore_changes = [name, description]
   }
@@ -46,10 +46,11 @@ data "aws_iam_policy_document" "vpc_cni" {
 resource "aws_eks_addon" "vpc_cni" {
   count = var.addon_create_vpc_cni ? 1 : 0
 
-  cluster_name      = var.eks_cluster_id
-  addon_name        = "vpc-cni"
-  resolve_conflicts = "OVERWRITE"
-  addon_version     = var.addon_vpc_cni_version
+  cluster_name             = var.eks_cluster_id
+  addon_name               = "vpc-cni"
+  resolve_conflicts        = "OVERWRITE"
+  addon_version            = var.addon_vpc_cni_version
+  service_account_role_arn = module.irsa_vpc_cni.iam_role_arn
 
   lifecycle {
     ignore_changes = [cluster_name, addon_name]
--- variables.tf
+++ variables.tf
@@ -31,13 +31,13 @@ variable "addon_vpc_cni_version" {
 }
 
 variable "addon_kube_proxy_version" {
-  default     = ""
+  default     = "v1.20.7-eksbuild.1"
   description = "Version for addon_kube_proxy_version"
 }
 
 
 variable "addon_coredns_version" {
-  default     = ""
+  default     = "v1.8.3-eksbuild.1"
   description = "Version for addon_coredns_version"
 }
 

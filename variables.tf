88d757d5f99c59d3c6a873a61aea6ddaafa45b78
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2022-03-21 14:54:29 +0000

    Update addon_kube_proxy_version to right version

--- variables.tf
+++ variables.tf
@@ -32,7 +32,7 @@ variable "addon_vpc_cni_version" {
 }
 
 variable "addon_kube_proxy_version" {
-  default     = "v1.21.2-eksbuild.1"
+  default     = "v1.21.2-eksbuild.2"
   description = "Version for addon_kube_proxy_version"
   type = string
 }

b96701164108e6ec731be29f95c14cebdf589847
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2022-04-21 12:07:56 +0300

    increase number of coredns replicas (#8)
    * increase number of coredns replicas
    
    Per the hint in https://console.aws.amazon.com/support/home?region=eu-west-2#/case/?displayId=9737685701&language=en to user reported intermittent DNS failures in live cluster
    
    * increase count of coredns pods
    
    * prevent scaling to 0 by mistake

--- main.tf
+++ main.tf
@@ -106,3 +106,21 @@ resource "aws_eks_addon" "coredns" {
 
   tags = var.addon_tags
 }
+
+# Increase number of coredns pods
+
+# This null_resource can be removed when Kibana doesn't show "could not translate host name" events anymore
+# https://kibana.cloud-platform.service.justice.gov.uk/_plugin/kibana/goto/4729982d70f22c38fce1a5e6ba2efa96
+# 5 is very much a magic number obtained by manual editing and watching the graph at
+# https://grafana.live.cloud-platform.service.justice.gov.uk/d/vkQ0UHxik/coredns?orgId=1
+resource "null_resource" "more_coredns_pods" {
+  depends_on = [ aws_eks_addon.coredns ]
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      aws eks --region eu-west-2 update-kubeconfig --name ${var.cluster_name}
+      count=$(kubectl get nodes | grep Ready | wc -l) ; let count/=5 ; [ $count -lt 2 ] && count=2
+      kubectl -n kube-system scale deployment coredns --replicas=$count
+    EOT
+  }
+}
